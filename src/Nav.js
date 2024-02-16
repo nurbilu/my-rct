11193fbed6e229d11167aa19fdaa69cd0586fafe
nurbilu <nurb111@gmail.com>
2024-02-16 16:35:01 +0200

    4th commit

--- src/Nav.js
+++ src/Nav.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Offcanvas, Button } from 'react-bootstrap';
 
 function Nav() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/cart">My Cart</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-        <li><Link to="/settle">Settle</Link></li>
-      </ul>
-    </nav>
+    <div>
+      <Button variant="primary" onClick={handleShow}>
+        Menu
+      </Button>
+      <br/><br/>
+
+      <Offcanvas show={show} onHide={handleClose}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title class>Navbar</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body class="nav flex-column nav nav-underline nav nav-tabs">
+          <Link to="/" type="button" class="btn btn-outline-dark" onClick={handleClose}>Home</Link><br/><br/>
+          <Link to="/cart" type="button" class="btn btn-outline-dark" onClick={handleClose}>My Cart</Link><br/><br/>
+          <Link to="/contact" type="button" class="btn btn-outline-dark" onClick={handleClose}>Contact</Link><br/><br/>
+          <Link to="/settle" type="button" class="btn btn-outline-dark" onClick={handleClose}>Settle</Link>
+        </Offcanvas.Body>
+      </Offcanvas>
+    </div>
   );
 }
 
 export default Nav;
+
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { CartProvider } from './UserContext'; // Assuming UserProvider is the context provider component
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
